2f8f3161b33d59ac36482ea8cadfc550b1d3a6b6
Charandeep S. Matta <charandeep.matta@gmail.com>
2017-06-22 18:21:56 +0300

    drop banner

--- src/main/java/com/charandeepmatta/sam/SamApplication.java
+++ src/main/java/com/charandeepmatta/sam/SamApplication.java
@@ -1,12 +1,16 @@
 package com.charandeepmatta.sam;
 
+import static org.springframework.boot.Banner.Mode.OFF;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SamApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SamApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication app = new SpringApplication(SamApplication.class);
+    app.setBannerMode(OFF);
+    app.run(args);
+  }
 }
