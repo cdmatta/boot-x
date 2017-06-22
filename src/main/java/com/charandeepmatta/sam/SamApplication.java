368ca36c451f58db4df2b39d027fccc390a44086
Charandeep S. Matta <charandeep.matta@gmail.com>
2017-06-22 18:15:52 +0300

    spring starter project

---
+++ src/main/java/com/charandeepmatta/sam/SamApplication.java
@@ -0,0 +1,12 @@
+package com.charandeepmatta.sam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamApplication.class, args);
+	}
+}
